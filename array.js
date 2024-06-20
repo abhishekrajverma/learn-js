8e5fb4fc1d38f04f9aa64055da2e3f7d0a1d7c35
Abhishek Raj <abhishekr359@gmail.com>
2024-06-20 21:09:03 +0530

    array methods

---
+++ array.js
@@ -0,0 +1,27 @@
+const arr = [1,2,3,4,5,6] // array of numbers 
+
+const arr2 = new Array(1,2,3,4,5,6) // another way to create an array of numbers
+
+
+// array methods
+
+// arr.push(7,8) // add element to the end of the array 
+// console.log(arr)
+
+// arr.pop() // remove element from the end of the array
+// console.log(arr)
+
+// arr.unshift(74) //  add element to the beginning of the array 
+// console.log(arr)
+
+// arr.shift() // remove element from the beginning of the array
+// console.log(arr)
+
+// console.log(arr.includes(3)) // check if the array includes the element it returns true or false 
+// let arr1 = arr.includes(3) // another way to check if the array includes the element it returns true or false 
+// console.log(arr1)
+
+// console.log(arr.indexOf(3)) // returns the index of the element in the array if it exists otherwise it returns -1
+
+
+
